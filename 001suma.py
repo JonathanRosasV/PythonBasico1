de308ddef7152399e333c11737d1dd1edcd59440
Jonathan R. <127298565+JonathanRosasV@users.noreply.github.com>
2024-10-26 07:44:52 -0500

    Add files via upload

---
+++ 001suma.py
@@ -0,0 +1,5 @@
+# Este programa sumará 2 números y mostrará el resultado
+n1=5
+n2=6
+resultado=n1+n2
+print('El resultado de la suma es: ', resultado)
---
+++ 002suma.py
@@ -0,0 +1,9 @@
+# Entrada de datos
+n1=int(input('Escribe el primer dato a sumar: '))
+n2=int(input('Escribe el segundo dato a sumar: '))
+
+# Procesamiento de datos
+resultado=n1+n2
+
+# Salida de datos
+print('El resultado de la suma es: ', resultado)
