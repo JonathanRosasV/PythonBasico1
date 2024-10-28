7777f34b5431361c1f32211f9d40f54e721e6c44
Jonathan R. <127298565+JonathanRosasV@users.noreply.github.com>
2024-10-28 09:32:19 -0500

    Delete 003suma.py

--- 003suma.py
+++
@@ -1,7 +0,0 @@
-# Sumar datos con decimales
-
-n1=float(input('Escribe el primer dato a sumar: '))
-n2=float(input('Escribe el segundo dato a sumar: '))
-
-resultado=n1+n2
-print('El resultado de la suma es: ', resultado)
