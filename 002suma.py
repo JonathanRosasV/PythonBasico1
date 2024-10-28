6accb976b792b5a9737501495bcf9e905436f450
Jonathan R. <127298565+JonathanRosasV@users.noreply.github.com>
2024-10-28 09:24:59 -0500

    Delete 002suma.py

--- 002suma.py
+++
@@ -1,9 +0,0 @@
-# Entrada de datos
-n1=int(input('Escribe el primer dato a sumar: '))
-n2=int(input('Escribe el segundo dato a sumar: '))
-
-# Procesamiento de datos
-resultado=n1+n2
-
-# Salida de datos
-print('El resultado de la suma es: ', resultado)
